a3acd43512be22ebf66979c086f41cf1ecc6f783
Geovane Junior <geovanejr.dev@gmail.com>
2022-08-29 10:48:43 -0300

    Corrigindo a main

--- src/main/java/br/gov/sp/ferrazdevasconcelos/protocolmanager/ProtocolManagerApplication.java
+++ src/main/java/br/gov/sp/ferrazdevasconcelos/protocolmanager/ProtocolManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GerenciadorDeProtocolosApplication {
+public class ProtocolManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GerenciadorDeProtocolosApplication.class, args);
+		SpringApplication.run(ProtocolManagerApplication.class, args);
 	}
 
 }
